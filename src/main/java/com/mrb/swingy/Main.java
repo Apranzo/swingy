e1503c8d8e9cb643e848e4b1c411e6edcabd55e3
MrBlackk <volodymyr.chornyj@gmail.com>
2018-06-18 17:32:44 +0300

    - added usage, and parsing args

--- src/main/java/com/mrb/swingy/Main.java
+++ src/main/java/com/mrb/swingy/Main.java
@@ -6,7 +6,13 @@ import com.mrb.swingy.view.start.StartViewGUI;
 public class Main {
 
     public static void main(String[] args) {
-//        new StartViewConsole().start();
-        new StartViewGUI().start();
+        if (args.length != 1 || (!args[0].equals("console") && !args[0].equals("gui"))){
+            System.out.println("Usage: program console | gui");
+            System.exit(1);
+        }
+        if (args[0].equals("console"))
+            new StartViewConsole().start();
+        else if (args[0].equals("gui"))
+            new StartViewGUI().start();
     }
 }
