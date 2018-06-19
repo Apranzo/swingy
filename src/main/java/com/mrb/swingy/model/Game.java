4c486748481ede47684f4c09e52c63850443e7fc
MrBlackk <volodymyr.chornyj@gmail.com>
2018-06-19 12:51:02 +0300

    - added base game class

---
+++ src/main/java/com/mrb/swingy/model/Game.java
@@ -0,0 +1,14 @@
+package com.mrb.swingy.model;
+
+import com.mrb.swingy.model.character.Hero;
+import com.mrb.swingy.util.Point;
+
+/**
+ * Created by chvs on 19.06.2018.
+ */
+public class Game {
+
+    private Hero hero;
+    private Point heroCoord;
+    private boolean[][] map;
+}
